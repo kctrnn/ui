91c8e2a77ca71010e6c8185fd5b9711f1f805769
kctrnn <kctrnn@gmail.com>
2025-03-15 11:06:51 +0700

    adding button component

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,13 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import { Button } from './components/ui/button.tsx'
+
 import './index.css'
-import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <div className="flex flex-col items-center justify-center min-h-svh">
+      <Button>Click me</Button>
+    </div>
   </StrictMode>,
 )
