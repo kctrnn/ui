0432a391965b197d9b3294afc82860f0916c7673
kctrnn <kctrnn@gmail.com>
2025-03-31 01:24:53 +0700

    library mode and publish to npm v1.0.0

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,42 @@
+import { dirname, resolve } from 'node:path'
+import { fileURLToPath } from 'node:url'
+import path from 'path'
+
 import tailwindcss from '@tailwindcss/vite'
 import react from '@vitejs/plugin-react'
-import path from 'path'
 import { defineConfig } from 'vite'
+import dts from 'vite-plugin-dts'
+
+const __dirname = dirname(fileURLToPath(import.meta.url))
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [
+    react(),
+    tailwindcss(),
+    dts({ rollupTypes: true, tsconfigPath: './tsconfig.app.json' }),
+  ],
+
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
+    },
+  },
+
+  build: {
+    lib: {
+      entry: resolve(__dirname, 'src/components/index.ts'),
+      name: 'ui',
+    },
+
+    rollupOptions: {
+      external: ['react', 'react-dom'],
+      output: {
+        globals: {
+          react: 'React',
+          'react-dom': 'ReactDOM',
+        },
+      },
     },
   },
 })
