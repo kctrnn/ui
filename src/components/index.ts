ec357380096a7a53dc23ed399cb4e1501954c9ca
kctrnn <kctrnn@gmail.com>
2025-04-08 01:20:29 +0700

    1.0.8

--- src/components/index.ts
+++ src/components/index.ts
@@ -1,5 +1,3 @@
-"use client"
-
 import '../index.css'
 
 export * from './accordion'
--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,7 +1,5 @@
-"use client"
-
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { clsx, type ClassValue } from 'clsx'
+import { twMerge } from 'tailwind-merge'
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
